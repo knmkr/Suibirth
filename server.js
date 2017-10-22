eb2eca1214b26149d944b0b4110695f2a3ea74e1
Espressomilk <jack.shi2013@gmail.com>
2017-10-22 01:39:44 -0700

    Implement monitering dangerous areas from Database table danger_spot Edit .gitignore file Clean code

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+// Twilio Sid and Token
+var accountSid = 'ACa2955fdf0ae0cd73667ebabff5149f46'; // Your Account SID from www.twilio.com/console
+var authToken = '3ad78b221e2ef261c6a03d122f2394b8'; // Your Auth Token from www.twilio.com/console
+var myphonenumber = '+14159660950'
+var twilio = require('twilio');
+
 var urlencodedParser = bodyParser.urlencoded({
 	extended: false
 })
@@ -33,6 +39,7 @@ String.prototype.trim = function() {
 	return str.slice(0, i + 1);
 }
 
+// Genome Link Token
 const request = require("request");
 const token = "SqjcJmik2kzGfgxZ0kApmht9z51gWV"
 const options = {
@@ -62,7 +69,6 @@ function isUsernameExisted(username, callback) {
 		if (err) {
 			callback(err, null);
 		} else {
-			// console.log(Object.keys(result).length);
 			if (Object.keys(result).length) callback(null, result[0].password);
 			else callback(null, '');
 		}
@@ -177,10 +183,6 @@ app.post('/update', urlencodedParser, function(req, res) {
 	});
 });
 
-var accountSid = 'ACa2955fdf0ae0cd73667ebabff5149f46'; // Your Account SID from www.twilio.com/console
-var authToken = '3ad78b221e2ef261c6a03d122f2394b8'; // Your Auth Token from www.twilio.com/console
-var myphonenumber = '+14159660950'
-var twilio = require('twilio');
 
 var client = new twilio(accountSid, authToken);
 
@@ -192,7 +194,6 @@ function sendAlert(result) {
 	console.log(result);
 	function step1(result, callback) {
 		result.forEach(function(value, index, array) {
-			// console.log(value);
 			if(value.score > 5) {
 				victim_detected.push(value.username);
 				victim_location.push(value.l);
@@ -207,8 +208,8 @@ function sendAlert(result) {
 		console.log('oth:', others_detected);
 		if(victim_detected.length != 0) {
 			for(var i = 0; i < victim_detected.length; ++i) {
-				// console.log("Sending to victim");
 				var to = ('+1' + victim_detected[i]).trim();
+				console.log("Sending to victim", to);
 				client.calls.create({
 					url: "http://108.61.247.112/public/translate_tts.xml",
 					to: to, // Text this number
@@ -219,13 +220,13 @@ function sendAlert(result) {
 			}
 			console.log(">> VICTIM LOCATION: ", victim_location);
 			for(var i = 0; i < others_detected.length; ++i) {
-				console.log("Sending to others");
 				var to = ('+1' + others_detected[i]).trim();
+				console.log("Sending to others", to);
 				var longtitute = victim_location[0].split('(')[1].split(' ')[0].trim()
 				var latitute = victim_location[0].split(' ')[1].split(')')[0].trim()
 				var locUrl = 'https://www.google.com/maps/search/?api=1&query=' + longtitute + ',' + latitute;
-				// console.log(locUrl);
-				// console.log(to);
+				console.log(locUrl);
+				console.log(to);
 				client.messages.create({
 					body: 'ALERT: Someone around you may want to commit suicide. Location: ' + locUrl,
 					to: to, // Text this number
@@ -241,20 +242,24 @@ function sendAlert(result) {
 }
 
 function checkEndanger() {
-	connection.query("SET @poly=?", 'Polygon((32.892764 -117.247124,32.869446 -117.247339,32.870275 -117.217384,32.891935 -117.223907,32.892764 -117.247124))', function(err, result) {
-		if (err) return;
-		connection.query("SELECT user_location.username, ST_AsText(location) as l, score FROM user_location LEFT JOIN users ON user_location.username=users.username WHERE MBRContains(ST_GeomFromText(@poly), location)", function(err, result) {
-			if (err) {
-				console.log('[SELECT ERROR] - ', err.message);
-				return;
-			}
-			if (Object.keys(result).length) {
-				console.log('1111');
-				sendAlert(result);
-
-			}
+	connection.query("SELECT ST_AsText(area) as area FROM danger_spot", function(err, result) {
+		if(err) return;
+		result.forEach(function(value, index, array) {
+			connection.query("SET @poly=?", value.area, function(err, result) {
+				if (err) return;
+				connection.query("SELECT user_location.username, ST_AsText(location) as l, score FROM user_location LEFT JOIN users ON user_location.username=users.username WHERE MBRContains(ST_GeomFromText(@poly), location)", function(err, result) {
+					if (err) {
+						console.log('[SELECT ERROR] - ', err.message);
+						return;
+					}
+					if (Object.keys(result).length) {
+						sendAlert(result);
+					}
+				});
+			});
 		});
 	});
+	
 }
 
 var checkInterval = setInterval(checkEndanger, 1000, "Interval");
